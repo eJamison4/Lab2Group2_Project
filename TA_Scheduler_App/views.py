a0b7290db89034a577a50040834ee8d600fa7d70
Evan Jamison <jamison4@uwm.edu>
2025-04-09 20:24:19 -0500

    setup for login page ui

--- TA_Scheduler_App/views.py
+++ TA_Scheduler_App/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class Login(View):
+    def get(self, request):
+        ...
+
+    def post(self, request):
+        ...
+
+
+class Account(view):
+    def get(self, request):
+        ...
+
+    def post(self, request):
+        ...
