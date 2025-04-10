165f3b8f0b72acdd4219cea8e2feffb27a76b8e8
Evan Jamison <jamison4@uwm.edu>
2025-04-10 17:14:40 -0500

    Got login page ready for backend integration

--- TA_Scheduler_App/views.py
+++ TA_Scheduler_App/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 # Create your views here.
 class Login(View):
     def get(self, request):
-        ...
+        return render(request, "login.html", {})
 
     def post(self, request):
-        ...
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if username.strip() == "" or password.strip() == "":  # do nothing if either field is empty
+            return render(request, "login.html")
+        print(username)
+        print(password)
+        return render(
+            request,
+            "login.html",
+            {"message": "Either username or password is incorrect!"}
+        )
+        # return redirect()
 
 
-class Account(view):
+class Account(View):
     def get(self, request):
         ...
 
