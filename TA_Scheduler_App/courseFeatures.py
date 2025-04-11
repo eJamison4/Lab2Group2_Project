32382ad02493c6eabd1a79dda167196c0e42b048
Jack <jasamson@uwm.edu>
2025-04-11 15:51:58 -0500

    made methods

---
+++ TA_Scheduler_App/courseFeatures.py
@@ -0,0 +1,40 @@
+import models
+
+class CourseFeatures:
+
+    @staticmethod
+    def create_course(self, courseName):
+        pass
+
+    @staticmethod
+    def create_section(self, courseForeignKey, sectionData):
+        pass
+
+    @staticmethod
+    def create_lab(self, courseForeignKey, labData):
+        pass
+
+    @staticmethod
+    def delete_course(self, courseKey):
+        pass
+
+    @staticmethod
+    def delete_section(self, sectionKey):
+        pass
+
+    @staticmethod
+    def delete_lab(self, labKey):
+        pass
+
+    @staticmethod
+    def edit_course(self, courseKey):
+        pass
+
+    @staticmethod
+    def edit_section(self, sectionKey):
+        pass
+
+    @staticmethod
+    def edit_lab(self, labKey):
+        pass
+
