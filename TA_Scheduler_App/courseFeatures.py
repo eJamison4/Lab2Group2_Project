5c86619a34eb959e7c31309f903a4402ebc47215
jackSamson <jasamson@uwm.edu>
2025-04-14 11:46:46 -0500

    some more methods

--- TA_Scheduler_App/courseFeatures.py
+++ TA_Scheduler_App/courseFeatures.py
@@ -5,16 +5,19 @@ class CourseFeatures:
     @staticmethod
     def create_course(self, courseName):
         course = models.Course.objects.create(name=courseName)
+        course.save()
         return course
 
     @staticmethod
     def create_section(self, courseForeignKey, sectionData):
         section = models.Section.objects.create(courseForeignKey=courseForeignKey, sectionTime=sectionData)
+        section.save()
         return section
 
     @staticmethod
     def create_lab(self, courseForeignKey, labData):
         lab = models.Lab.objects.create(courseForeignKey=courseForeignKey, labTime=labData)
+        lab.save()
         pass
 
     @staticmethod
@@ -29,11 +32,22 @@ class CourseFeatures:
 
     @staticmethod
     def delete_section(self, sectionKey):
-        pass
+        try:
+            section = models.Section.objects.get(key=sectionKey)
+            section.delete()
+            return True
+        except models.Section.DoesNotExist:
+            return False
 
     @staticmethod
     def delete_lab(self, labKey):
-        pass
+        try:
+            lab = models.Lab.objects.get(key=labKey)
+            lab.delete()
+            return True
+
+        except models.Lab.DoesNotExist:
+            return False
 
     @staticmethod
     def edit_course(self, courseKey):
