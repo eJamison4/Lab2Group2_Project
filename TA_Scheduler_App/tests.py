6ad461683cb7ecfa303cbaa97a2ae482bedeee3d
jackSamson <jasamson@uwm.edu>
2025-04-14 18:59:00 -0500

    making tests for course functions

--- TA_Scheduler_App/tests.py
+++ TA_Scheduler_App/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.db import models
+from courseFeatures import CourseFeatures
+from models import Section, Course, Lab
 
 # Create your tests here.
+
+class testCourseFeatures(TestCase):
+
+    def setUp(self):
+        self.service = CourseFeatures()
+
+    def testCourseCreation(self):
+
+        pass
+
+    def testSectionCreation(self):
+        pass
+
+    def testLabCreation(self):
+        pass
+
+    def testCourseDelete(self):
+        pass
+
+    def testSectionDelete(self):
+        pass
+
+    def testLabDelete(self):
+        pass
+
+    def testCourseUpdate(self):
+        pass
+
+    def testSectionUpdate(self):
+        pass
+
+    def testLabUpdate(self):
+        pass
+
+    pass
