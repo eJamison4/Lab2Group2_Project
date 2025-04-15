4c7b4ce4d2f2af1c432a53b87a70ef287e3ee160
Jack <jasamson@uwm.edu>
2025-04-15 12:51:29 -0500

    update imports and tests

--- TA_Scheduler_App/tests.py
+++ TA_Scheduler_App/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.db import models
 from TA_Scheduler_App.models import Section, Course, Lab
 from TA_Scheduler_App.courseFeatures import CourseFeatures
 
@@ -11,19 +10,21 @@ class testCourseFeatures(TestCase):
         self.service = CourseFeatures()
 
     def testCourseCreation(self):
-        course = Course.objects.create(name="yugioh class")
+        course = self.service.create_course(self,courseName="yugioh class")
 
         self.assertEqual(course.courseName, 'yugioh class')
         self.assertEqual(Course.objects.count(), 1)
 
 
     def testSectionCreation(self):
-        section = self.service.createSection('time to duel')
+        course = self.service.create_course(self,courseName="yugioh class")
+
+        section = self.service.create_section(self,course.pk,'time to duel')
         self.assertEqual(section.sectionTime, 'time to duel')
         self.assertEqual(Section.objects.count(), 1)
 
     def testLabCreation(self):
-        lab = self.service.createLab('time to duel')
+        lab = self.service.create_lab('time to duel')
         self.assertEqual(lab.labTime, 'time to duel')
         self.assertEqual(Lab.objects.count(), 1)
         pass
