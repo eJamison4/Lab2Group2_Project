d4ca42d49d182a28125d87d98e7fb86e8ec4ec4c
Jack <jasamson@uwm.edu>
2025-04-15 12:51:06 -0500

    update imports and methods

--- TA_Scheduler_App/courseFeatures.py
+++ TA_Scheduler_App/courseFeatures.py
@@ -1,60 +1,59 @@
-from django.db import models
-import TA_Scheduler_App.models
+from TA_Scheduler_App.models import Lab,Section,Course
 
 class CourseFeatures:
 
     @staticmethod
     def create_course(self, courseName):
-        course = models.Course.objects.create_course(name=courseName)
+        course = Course.objects.create(name=courseName)
         course.save()
         return course
 
     @staticmethod
     def create_section(self, courseForeignKey, sectionData):
-        section = models.Section.objects.create(courseForeignKey=courseForeignKey, sectionTime=sectionData)
+        section = Section.objects.create(courseForeignKey=courseForeignKey, sectionTime=sectionData)
         section.save()
         return section
 
     @staticmethod
     def create_lab(self, courseForeignKey, labData):
-        lab = models.Lab.objects.create(courseForeignKey=courseForeignKey, labTime=labData)
+        lab = Lab.objects.create(courseForeignKey=courseForeignKey, labTime=labData)
         lab.save()
         pass
 
     @staticmethod
     def delete_course(self, courseKey):
         try:
-            course = models.Course.objects.get(key=courseKey)
+            course = Course.objects.get(key=courseKey)
             course.delete()
             return True
-        except models.Course.DoesNotExist:
+        except Course.DoesNotExist:
             return False
 
 
     @staticmethod
     def delete_section(self, sectionKey):
         try:
-            section = models.Section.objects.get(key=sectionKey)
+            section = Section.objects.get(key=sectionKey)
             section.delete()
             return True
-        except models.Section.DoesNotExist:
+        except Section.DoesNotExist:
             return False
 
     @staticmethod
     def delete_lab(self, labKey):
         try:
-            lab = models.Lab.objects.get(key=labKey)
+            lab = Lab.objects.get(key=labKey)
             lab.delete()
             return True
 
-        except models.Lab.DoesNotExist:
+        except Lab.DoesNotExist:
             return False
 
     @staticmethod
     def edit_course(self, courseKey, newCourseName=None):
         try:
-            course = models.Course.objects.get(key=courseKey)
-        except models.Course.DoesNotExist:
+            course = Course.objects.get(key=courseKey)
+        except Course.DoesNotExist:
             return False
 
         if newCourseName is not None:
@@ -66,8 +65,8 @@ class CourseFeatures:
     @staticmethod
     def edit_section(self, sectionKey, newSectionTime=None):
         try:
-            section = models.Section.objects.get(key=sectionKey)
-        except models.Section.DoesNotExist:
+            section = Section.objects.get(key=sectionKey)
+        except Section.DoesNotExist:
             return False
 
         if newSectionTime is not None:
@@ -81,8 +80,8 @@ class CourseFeatures:
     @staticmethod
     def edit_lab(self, labKey, newLabTime=None):
         try:
-            lab = models.Lab.objects.get(key=labKey)
-        except models.Lab.DoesNotExist:
+            lab = Lab.objects.get(key=labKey)
+        except Lab.DoesNotExist:
             return False
 
         if newLabTime is not None:
