824776cc1446e737680b6411e9a9094e152d8554
jackSamson <jasamson@uwm.edu>
2025-04-20 21:59:17 -0500

    type hinting and a few more trys

--- TA_Scheduler_App/courseFeatures.py
+++ TA_Scheduler_App/courseFeatures.py
@@ -3,25 +3,25 @@ from TA_Scheduler_App.models import Lab,Section,Course
 class CourseFeatures:
 
     @staticmethod
-    def create_course(self, courseName):
+    def create_course(self, courseName:str):
         course = Course.objects.create(courseName=courseName)
         course.save()
         return course
 
     @staticmethod
-    def create_section(self, courseForeignKey, sectionData):
+    def create_section(self, courseForeignKey:Course, sectionData:str):
         section = Section.objects.create(courseForeignKey=courseForeignKey, sectionTime=sectionData)
         section.save()
         return section
 
     @staticmethod
-    def create_lab(self, courseForeignKey, labData):
+    def create_lab(self, courseForeignKey:Course, labData:str):
         lab = Lab.objects.create(courseForeignKey=courseForeignKey, labTime=labData)
         lab.save()
         return lab
 
     @staticmethod
-    def delete_course(self, courseKey):
+    def delete_course(self, courseKey:int):
         try:
             course = Course.objects.get(pk=courseKey)
             course.delete()
@@ -31,7 +31,7 @@ class CourseFeatures:
 
 
     @staticmethod
-    def delete_section(self, sectionKey):
+    def delete_section(self, sectionKey:int):
         try:
             section = Section.objects.get(pk=sectionKey)
             section.delete()
@@ -40,7 +40,7 @@ class CourseFeatures:
             return False
 
     @staticmethod
-    def delete_lab(self, labKey):
+    def delete_lab(self, labKey:int):
         try:
             lab = Lab.objects.get(pk=labKey)
             lab.delete()
@@ -50,8 +50,12 @@ class CourseFeatures:
             return False
 
     @staticmethod
-    def edit_course(self, courseKey, newCourseName=""):
-        course = Course.objects.get(pk=courseKey)
+    def edit_course(self, courseKey:int, newCourseName:str=""):
+        try:
+            course = Course.objects.get(pk=courseKey)
+        except Course.DoesNotExist:
+            return False
+
 
         if newCourseName is not '' and newCourseName is not None:
             course.courseName = newCourseName
@@ -60,8 +64,11 @@ class CourseFeatures:
         return course
 
     @staticmethod
-    def edit_section(self, sectionKey, newSectionTime=None):
-        section = Section.objects.get(pk=sectionKey)
+    def edit_section(self, sectionKey:int, newSectionTime:str=None):
+        try:
+            section = Section.objects.get(pk=sectionKey)
+        except Section.DoesNotExist:
+            return False
 
         if newSectionTime is not None and newSectionTime is not '':
             section.sectionTime = newSectionTime
@@ -71,8 +78,11 @@ class CourseFeatures:
 
 
     @staticmethod
-    def edit_lab(self, labKey, newLabTime=None):
-        lab = Lab.objects.get(pk=labKey)
+    def edit_lab(self, labKey:int, newLabTime:str=None):
+        try:
+            lab = Lab.objects.get(pk=labKey)
+        except Lab.DoesNotExist:
+            return False
 
         if newLabTime is not None and newLabTime is not '' :
             lab.labTime = newLabTime
