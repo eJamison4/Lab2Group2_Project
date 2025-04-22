eebdd3641b58019c8e7a05e0620097e4bd26f01d
dspin0 <lex.espino121@gmail.com>
2025-04-22 14:37:27 -0500

    Added accounts page and started implementation of accounts view

--- TA_Scheduler_App/views.py
+++ TA_Scheduler_App/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render  # , redirect
 from django.views import View
+from django.urls import re_path
+from TA_Scheduler_App.models import User
+from TA_Scheduler_App.account_features import AccountFeatures
+
 
 
 # Create your views here.
@@ -25,7 +30,20 @@ class Login(View):
 
 class Account(View):
     def get(self, request):
-        ...
+        users = User.objects.all()
+        return render(request, "accounts.html", {"users": users})
 
     def post(self, request):
-        ...
+        action = request.POST['action']
+
+        if action == "create":
+
+            AccountFeatures.create_user(username=request.POST['username'], password=request.POST['password']
+                                        , user_email=request.POST['userEmail'], first_name=request.POST['firstName'],
+                                        last_name=request.POST['lastName'], home_address=request.POST['homeAddress'],
+                                        account_type=request.POST['accountType'],)
+
+        elif action == "edit":
+            AccountFeatures.edit_account(username=request.POST['username'], password=request.POST['password'], user_email=request.POST['userEmail'],
+                                         first_name=request.POST['firstName'], last_name=request.POST['lastName'], home_address=request.POST['homeAddress'],
+                                         account_type=request.POST['accountType'],)
