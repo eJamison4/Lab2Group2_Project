5783766418bfd76d85103d50473156cc6a94d1b8
dspin0 <lex.espino121@gmail.com>
2025-04-22 17:59:39 -0500

    Added accounts url, changed up account views

--- TA_Scheduler_App/views.py
+++ TA_Scheduler_App/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render  # , redirect
+from django.shortcuts import render, redirect, reverse
 from django.views import View
 from TA_Scheduler_App.models import User
 from TA_Scheduler_App.account_features import AccountFeatures
@@ -34,30 +34,35 @@ class Login(View):
 
 
 class Account(View):
+    template_name = "accounts.html"
+
     def get(self, request):
-        users = User.objects.all()
-        return render(request, "accounts.html", {"users": users})
+        users = User.objects.all().order_by("id")
+        return render(request, self.template_name, {"users": users})
 
     def post(self, request):
-        action = request.POST['action']
+        action = request.POST.get('action')
 
         if action == "create":
 
-            AccountFeatures.create_user(username=request.POST['username'], password=request.POST['password']
-                                        , user_email=request.POST['userEmail'], first_name=request.POST['firstName'],
-                                        last_name=request.POST['lastName'], home_address=request.POST['homeAddress'],
-                                        account_type=request.POST['accountType'],)
+            AccountFeatures.create_user(username=request.POST.get('username'), password=request.POST.get('password')
+                                        , user_email=request.POST.get('userEmail'), first_name=request.POST.get('firstName'),
+                                        last_name=request.POST.get('lastName'), home_address=request.POST.get('homeAddress'),
+                                        account_type=int(request.POST.get('accountType')) or 0,)
 
         elif action == "edit":
 
-            user_id = request.POST['user_id']
+            user_id = request.POST.get("user_id")
 
-            AccountFeatures.edit_account(username=request.POST['username'], password=request.POST['password'], user_email=request.POST['userEmail'],
-                                         first_name=request.POST['firstName'], last_name=request.POST['lastName'], home_address=request.POST['homeAddress'],
-                                         account_type=request.POST['accountType'], user_id=user_id)
+            AccountFeatures.edit_account(username=request.POST.get('username') or None, password=request.POST.get('password') or None
+                                         ,user_email=request.POST.get('userEmail') or None,
+                                         first_name=request.POST.get('firstName') or None,
+                                         last_name=request.POST.get('lastName') or None
+                                         ,home_address=request.POST.get('homeAddress') or None,
+                                         account_type=request.POST.get('accountType') or None, user_id=user_id)
         elif action == "delete":
-            user_id = request.POST['user_id']
+            user_id = request.POST.get('user_id')
 
             AccountFeatures.delete_account(user_id=user_id)
 
-        return render(request, "accounts.html")
+        return redirect(reverse("accounts"))
