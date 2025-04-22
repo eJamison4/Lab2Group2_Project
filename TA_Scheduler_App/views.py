89704108e1845783149a9547c8efd0826aba6caa
dspin0 <lex.espino121@gmail.com>
2025-04-22 15:40:02 -0500

    Implemented views, ready to test

--- TA_Scheduler_App/views.py
+++ TA_Scheduler_App/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render  # , redirect
 from django.views import View
-from django.urls import re_path
 from TA_Scheduler_App.models import User
 from TA_Scheduler_App.account_features import AccountFeatures
 
@@ -18,8 +17,14 @@ class Login(View):
 
         if username.strip() == "" or password.strip() == "":  # do nothing if either field is empty
             return render(request, "login.html")
+        user = authenticate(request, username=username, password=password)
+
         print(username)
         print(password)
+        if user is not None:
+            login(request, user)
+            return render(request, "dashboard.html", context={"username": username})
+
         return render(
             request,
             "login.html",
@@ -44,6 +49,15 @@ class Account(View):
                                         account_type=request.POST['accountType'],)
 
         elif action == "edit":
+
+            user_id = request.POST['user_id']
+
             AccountFeatures.edit_account(username=request.POST['username'], password=request.POST['password'], user_email=request.POST['userEmail'],
                                          first_name=request.POST['firstName'], last_name=request.POST['lastName'], home_address=request.POST['homeAddress'],
-                                         account_type=request.POST['accountType'],)
+                                         account_type=request.POST['accountType'], user_id=user_id)
+        elif action == "delete":
+            user_id = request.POST['user_id']
+
+            AccountFeatures.delete_account(user_id=user_id)
+
+        return render(request, "accounts.html")
