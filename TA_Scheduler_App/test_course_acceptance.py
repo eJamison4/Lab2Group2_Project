dc1e10bcde82c5d84e0635cc25215e9442906135
Michael Perez <105227364+michael66549@users.noreply.github.com>
2025-04-23 16:26:03 -0500

    Update test_course_acceptance.py
    Signed-off-by: Michael Perez <105227364+michael66549@users.noreply.github.com>

--- TA_Scheduler_App/test_course_acceptance.py
+++ TA_Scheduler_App/test_course_acceptance.py
@@ -1,25 +1,118 @@
-from django.test import TestCase
-from TA_Scheduler_App.course_features import CourseFeatures
-from TA_Scheduler_App.models import Course
+from django.test import TestCase, Client
+from TA_Scheduler_App.models import Course, User
 
+class TestCourseViewAcceptance(TestCase):
+    def setUp(self):
+        self.client = Client()
 
-class CourseAcceptanceTests(TestCase):
-
-    def test_create_course_success(self):
-        # Basic course creation
-        course = CourseFeatures.create_course(
-            course_id="MATH101", course_title="Calculus I", course_description="Limits, derivatives, integrals"
+        # Admin user
+        self.admin_user = User.objects.create(
+            username="admin1",
+            password="adminpass",
+            userEmail="admin@email.com",
+            phoneNumber=1234567890,
+            firstName="Alice",
+            lastName="Admin",
+            homeAddress="1 Admin Rd",
+            accountType=2
         )
-        self.assertIsNotNone(course)
-        self.assertEqual(course.courseID, "MATH101")
-        self.assertEqual(course.courseTitle, "Calculus I")
-        self.assertEqual(course.courseDescription, "Limits, derivatives, integrals")
 
-    def test_create_course_duplicate_id(self):
-        # Try creating a duplicate course with the same ID
-        CourseFeatures.create_course("PHYS101", "Physics I", "Mechanics")
-        with self.assertRaises(Exception):  # Django will raise an IntegrityError unless handled
-            CourseFeatures.create_course("PHYS101", "Duplicate", "Should fail")
-    
-    def test_edit_existing_course_fields(self):
-        #TODO
+    def test_all_course_features_as_admin(self):
+        # Simulate admin login
+        session = self.client.session
+        session['_auth_user_id'] = self.admin_user.id
+        session.save()
+
+        # CREATE course
+        self.client.post('/courses/', {
+            "action": "create",
+            "courseName": "CS 101"
+        })
+        self.assertEqual(Course.objects.count(), 1, msg="Course should be created")
+
+        course = Course.objects.get(courseName="CS 101")
+
+        # EDIT course
+        self.client.post('/courses/', {
+            "action": "edit",
+            "courseId": course.id,
+            "newCourseName": "CS 102"
+        })
+        course.refresh_from_db()
+        self.assertEqual(course.courseName, "CS 102", msg="Course name should be updated")
+
+        # DELETE course
+        self.client.post('/courses/', {
+            "action": "delete",
+            "courseId": course.id,
+        })
+        self.assertEqual(Course.objects.count(), 0, msg="Course should be deleted")
+
+    def test_course_creation_rejected_for_non_admin(self):
+        ta_user = User.objects.create(
+            username="ta1",
+            password="tapass",
+            userEmail="ta@email.com",
+            phoneNumber=9876543210,
+            firstName="Tim",
+            lastName="Assistant",
+            homeAddress="2 TA Blvd",
+            accountType=0  # TA
+        )
+        session = self.client.session
+        session['_auth_user_id'] = ta_user.id
+        session.save()
+
+        response = self.client.post('/courses/', {
+            "action": "create",
+            "courseName": "CS 202"
+        })
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(Course.objects.count(), 0, msg="TA should not be able to create a course")
+
+    def test_course_edit_rejected_for_non_admin(self):
+        course = Course.objects.create(courseName="CS Unchanged")
+
+        ta_user = User.objects.create(
+            username="ta1",
+            password="tapass",
+            userEmail="ta@email.com",
+            phoneNumber=9876543210,
+            firstName="Tim",
+            lastName="Assistant",
+            homeAddress="2 TA Blvd",
+            accountType=0
+        )
+        session = self.client.session
+        session['_auth_user_id'] = ta_user.id
+        session.save()
+
+        response = self.client.post('/courses/', {
+            "action": "edit",
+            "courseId": course.id,
+            "newCourseName": "Hacked Name"
+        })
+        self.assertEqual(response.status_code, 403)
+        course.refresh_from_db()
+        self.assertEqual(course.courseName, "CS Unchanged", msg="Course name should not change")
+
+    def test_edit_course_with_blanks_does_nothing(self):
+        # Log in as admin and create course
+        session = self.client.session
+        session['_auth_user_id'] = self.admin_user.id
+        session.save()
+
+        self.client.post('/courses/', {
+            "action": "create",
+            "courseName": "Original Course"
+        })
+        course = Course.objects.get(courseName="Original Course")
+
+        # edit with blank name
+        self.client.post('/courses/', {
+            "action": "edit",
+            "courseId": course.id,
+            "newCourseName": ""
+        })
+        course.refresh_from_db()
+        self.assertEqual(course.courseName, "Original Course", msg="Blank edit should not change course name")
