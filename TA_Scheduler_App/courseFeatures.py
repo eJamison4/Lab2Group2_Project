75e7d525cfe62b06096d87dc0c80998156836133
jackSamson <jasamson@uwm.edu>
2025-04-30 15:45:10 -0500

    removed lab

--- TA_Scheduler_App/courseFeatures.py
+++ TA_Scheduler_App/courseFeatures.py
@@ -1,4 +1,4 @@
-from TA_Scheduler_App.models import Lab,Section,Course
+from TA_Scheduler_App.models import Section,Course
 
 class CourseFeatures:
 
@@ -14,11 +14,11 @@ class CourseFeatures:
         section.save()
         return section
 
-    @staticmethod
-    def create_lab(courseForeignKey:Course, labData:str):
-        lab = Lab.objects.create(courseForeignKey=courseForeignKey, labTime=labData)
-        lab.save()
-        return lab
+    # @staticmethod
+    # def create_lab(courseForeignKey:Course, labData:str):
+    #     lab = Lab.objects.create(courseForeignKey=courseForeignKey, labTime=labData)
+    #     lab.save()
+    #     return lab
 
     @staticmethod
     def delete_course(courseKey:int):
@@ -38,16 +38,16 @@ class CourseFeatures:
             return True
         except Section.DoesNotExist:
             return False
-
-    @staticmethod
-    def delete_lab(labKey:int):
-        try:
-            lab = Lab.objects.get(pk=labKey)
-            lab.delete()
-            return True
-
-        except Lab.DoesNotExist:
-            return False
+    #
+    # @staticmethod
+    # def delete_lab(labKey:int):
+    #     try:
+    #         lab = Lab.objects.get(pk=labKey)
+    #         lab.delete()
+    #         return True
+    #
+    #     except Lab.DoesNotExist:
+    #         return False
 
     @staticmethod
     def edit_course(courseKey:int, newCourseName:str=""):
@@ -76,17 +76,17 @@ class CourseFeatures:
 
         return section
 
-
-    @staticmethod
-    def edit_lab(labKey:int, newLabTime:str=None):
-        try:
-            lab = Lab.objects.get(pk=labKey)
-        except Lab.DoesNotExist:
-            return False
-
-        if newLabTime is not None and newLabTime is not '' :
-            lab.labTime = newLabTime
-            lab.save()
-
-        return lab
+    #
+    # @staticmethod
+    # def edit_lab(labKey:int, newLabTime:str=None):
+    #     try:
+    #         lab = Lab.objects.get(pk=labKey)
+    #     except Lab.DoesNotExist:
+    #         return False
+    #
+    #     if newLabTime is not None and newLabTime is not '' :
+    #         lab.labTime = newLabTime
+    #         lab.save()
+    #
+    #     return lab
 
